ccfb418af64058cc7dfb67e9c521fc0714250aac
Estellasy <siyiren1@foxmail.com>
2025-01-06 16:41:47 +0800

    todo

--- 说明.sh
+++ 说明.sh
@@ -6,3 +6,9 @@ python tools/dump_clip_features.py \
 
 # 生成OVD所需的特殊标注：
 python tools/get_coco_zeroshot_oriorder.py --data_path datasets/neudet/annotations/all.json --cat_path datasets/neudet/annotations/all.json
+
+
+进度说明：
+- 01-06 完成数据集准备，生成CLIP特征
+- 完成零样本分类器权重准备
+- 下一步：模型训练和知识蒸馏过程demo编写
